30342304b8c6eca05e0547993e49eee66eac2cd6
AdvenT <CSAdvenT@gmail.com>
2020-11-19 22:57:28 +0800

    Use correct HTTP status codes

--- handlers/student.go
+++ handlers/student.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	. "mtdn.io/44More/models"
 	"net/http"
 	"strconv"
@@ -13,13 +14,13 @@ func CreateStudentHandler(c *gin.Context) {
 	building := c.PostForm("building")
 	room, _ := strconv.ParseInt(c.PostForm("room"), 10, 64)
 	bed, _ := strconv.ParseInt(c.PostForm("bed"), 10, 64)
+	var status int
 	var msg string
-	var errorList string
-	var student *StudentModel
 	errorCount := 0
+	var student *StudentModel
 	if name == "" || building == "" || room == 0 || bed == 0 {
-		msg = "failed"
-		errorList += "not enough parameters\n"
+		status = http.StatusBadRequest
+		msg = "not enough parameters"
 		errorCount++
 	} else {
 		student = &StudentModel{
@@ -31,41 +32,45 @@ func CreateStudentHandler(c *gin.Context) {
 		}
 		_, err := student.CreateStudent()
 		if err == nil {
+			status = http.StatusOK
 			msg = "ok"
 		} else {
-			msg = "failed"
-			errorList += err.Error() + "\n"
+			status = http.StatusInternalServerError
+			msg = err.Error()
 			errorCount++
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(status, gin.H{
 		"msg":    msg,
 		"data":   student,
 		"error":  errorCount,
-		"errors": errorList,
 	})
 }
 
 func GetStudentHandler(c *gin.Context) {
 	stuno, _ := strconv.ParseInt(c.Param("stuno"), 10, 64)
 	student, err := GetStudent(stuno)
+	var status int
 	var msg string
-	var errorList string
 	errorCount := 0
 	if err == nil {
+		status = http.StatusOK
 		msg = "ok"
+	} else if err == gorm.ErrRecordNotFound {
+		status = http.StatusNotFound
+		msg = err.Error()
+		errorCount++
 	} else {
-		msg = "failed"
-		errorList += err.Error() + "\n"
+		status = http.StatusInternalServerError
+		msg = err.Error()
 		errorCount++
 	}
 
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(status, gin.H{
 		"msg":    msg,
 		"data":   student,
 		"error":  errorCount,
-		"errors": errorList,
 	})
 }
 
@@ -83,67 +88,76 @@ func UpdateStudentHandler(c *gin.Context) {
 		Room:     room,
 		Bed:      bed,
 	}
+	var status int
 	var msg string
-	var errorList string
 	errorCount := 0
 	_, err := student.UpdateStudent()
 	if err == nil {
+		status = http.StatusOK
 		msg = "ok"
 	} else {
-		msg = "failed"
-		errorList += err.Error()
+		status = http.StatusInternalServerError
+		msg = err.Error()
 		errorCount++
 	}
 
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(status, gin.H{
 		"msg":    msg,
 		"data":   student,
 		"error":  errorCount,
-		"errors": errorList,
 	})
 }
 
 func RemoveStudentHandler(c *gin.Context) {
 	stuno, _ := strconv.ParseInt(c.Param("stuno"), 10, 64)
 	_, err := RemoveStudent(stuno)
+	var status int
 	var msg string
-	var errorList string
 	errorCount := 0
 	if err == nil {
+		status = http.StatusOK
 		msg = "ok"
+	} else if err == gorm.ErrRecordNotFound {
+		status = http.StatusNotFound
+		msg = err.Error()
+		errorCount++
 	} else {
-		msg = "failed"
-		errorList += err.Error() + "\n"
+		status = http.StatusInternalServerError
+		msg = err.Error()
 		errorCount++
 	}
 
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(status, gin.H{
 		"msg":    msg,
 		"data":   stuno,
 		"error":  errorCount,
-		"errors": errorList,
 	})
 }
 
 func GetRoommateHandler(c *gin.Context) {
 	stuno, _ := strconv.ParseInt(c.Param("stuno"), 10, 64)
 	student, err := GetStudent(stuno)
+	var status int
 	var msg string
-	var errorList string
 	errorCount := 0
 	var roommates *[]StudentModel
 	if err == nil {
+		status = http.StatusOK
+		msg = "ok"
 		roommates = student.GetRoommate()
+	} else if err == gorm.ErrRecordNotFound {
+		status = http.StatusNotFound
+		msg = err.Error()
+		errorCount++
 	} else {
-		msg = "failed"
-		errorList += err.Error() + "\n"
+		status = http.StatusInternalServerError
+		msg = err.Error()
 		errorCount++
 	}
 
-	c.JSON(http.StatusOK, gin.H{
+	c.JSON(status, gin.H{
 		"msg":    msg,
 		"data":   roommates,
 		"error":  errorCount,
-		"errors": errorList,
 	})
 }
